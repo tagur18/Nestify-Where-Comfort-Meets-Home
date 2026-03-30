d42c44401d2c73363f41de3b69540dcdefd9f13a
TAGUR <tagurrathod@Tagurs-MAc.local>
2026-03-30 13:55:59 +0530

    final cleanup completed

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,27 +1,21 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');  
 
-
 cloudinary.config({
- cloud_name: process.env.CLOUD_NAME,
- api_key: process.env.CLOUD_API_KEY,
- api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'Air-bnb_DEV',
-<<<<<<< HEAD
-    allowedFormats:  ["png","jpg","jpeg"],
-=======
-    allowed_formats:  ["png","jpg","jpeg"],
->>>>>>> d2d455c (updated search bar text)
-
+    allowed_formats: ["png", "jpg", "jpeg"],
   },
 });
-module.exports = {
-    cloudinary,
-    storage,
-}
 
+module.exports = {
+  cloudinary,
+  storage,
+};
